4fd0fe64868a27d2843db5468786d3a385a1a1bf
Ilya <senior.smikow@yandex.ru>
2022-02-27 21:34:46 +0300

    Изменения по вёрстке

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,28 +1,10 @@
 import styles from "./Feedback.module.scss";
-import { useState } from "react";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 export const Feedback = ({ text, name, user }) => {
-  const [show, setShow] = useState(false);
-
-  const toggleShow = () => {
-    setShow(!show);
-  };
-
   return (
     <>
       <div className={styles.feedback__comment_wrapper}>
-        <div
-          className={
-            show ? styles.slider__full_comment : styles.slider__comment_text
-          }
-        >
-          {text}
-        </div>
-        <button onClick={toggleShow}>
-          {show ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-        </button>
+        <div className={styles.slider__comment_text}>{text}</div>
       </div>
       <div className={styles.feedback__user_wrapper}>
         <div className={styles.feedback__comment_name}>{name},</div>
